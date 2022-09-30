c47b5b538750cb4b5114d57910d95db984ae9b0c
gerardo montivero <gerardo.montivero@gmail.com>
2022-09-30 16:15:47 -0300

    :construction: tipos de datos

---
+++ jsmoderno/03-tipos-datos.js
@@ -0,0 +1,48 @@
+//string
+
+let nombre = "Nombre"
+
+// number
+
+let numero = 14;
+
+// boolean
+
+let mayor = true;
+let mayor_edad = 1;
+
+let menor = false;
+let menor_edad = Boolean(0);
+
+// array
+
+let countries = [
+    'España',
+    'Colombia',
+    'Argentina',
+]
+
+// null
+
+let nulo = null;
+
+// undefined
+
+let indefinido = undefined;
+
+
+// json
+
+
+
+console.log(
+    typeof nombre,
+    typeof numero,
+    typeof mayor,
+    typeof mayor_edad,
+    typeof menor,
+    typeof menor_edad,
+    typeof countries,
+    typeof nulo,
+    typeof indefinido,
+    );
