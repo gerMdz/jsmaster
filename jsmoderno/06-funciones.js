06dfbae8206a9cda6cc7350ae6a589718f4dfa44
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2023-02-16 08:29:30 -0300

    :heavy_plus_sign: Funciones de flecha

--- jsmoderno/06-funciones.js
+++ jsmoderno/06-funciones.js
@@ -11,3 +11,23 @@ console.log(saludo('Ger'));
 console.log(saludo('Ger', 2020));
 
 // Funciones flecha
+
+let saludo2 = function (nombre){
+    return "Hola " + nombre
+}
+console.log(saludo2("Ger MDz"))
+
+let saludo3 = nombre => "De nuevo por aquí " + nombre;
+
+console.log(saludo3("GJM"))
+
+let saludo4 = (nombre, pais) => "De nuevo por aquí " + nombre + " desde " + pais;
+
+console.log(saludo4("Julio", "ARG" ))
+
+let saludo5 = (nombre, pais) => {
+    let continente = "América";
+    return "De nuevo por aquí " + nombre + " desde " + pais + "(" + continente + ")";
+}
+
+console.log(saludo5("Julio", "ARG" ))
