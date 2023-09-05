e8ac33a406fff112559d45c595ac41e5258d7b9a
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2023-09-05 09:39:02 -0300

    :pencil2: Parte 3 de promesas con then

--- jsmoderno/09-promesas.js
+++ jsmoderno/09-promesas.js
@@ -29,12 +29,63 @@ getProducts = () => {
 
 }
 
+
+function procesarVenta(sale) {
+    console.log("Producto comprado: ", sale)
+
+    products = products.filter(
+        product => {
+            return product.name !== sale.producto
+        }
+    )
+
+    return products;
+}
+
+function purchasedProduct(name) {
+    return new Promise((resolve, reject) => {
+        console.log('Comprando...')
+        setTimeout(() => {
+            resolve({
+                producto: name,
+                cliente: 'GerMdz'
+            })
+        }, 1000)
+    })
+}
 getProducts()
     .then((items) => {
-        console.log(items)
+
+        console.log("Disponibles: " , items)
+        return purchasedProduct('Monitor Grande')
+    })
+    // .then(sale => {
+    //     procesarVenta(sale)
+    //     return purchasedProduct('Monitor Mediano')
+    // })
+    .then(sale => {
+        procesarVenta(sale)
+        return purchasedProduct('Monitor Chico')
+    })
+    .then(sale => {
+        procesarVenta(sale)
     })
     .catch(error => console.log(error.message))
-    .finally(() => console.log('Listo'))
+    .finally(() => {
+        if(products.length >= 1){
+            console.log("Disponibles: " , products)
+        }else{
+            console.log("Sin productos Disponibles")
+        }
+
+
+        console.log('... listo')
+
+    })
+
+//
 //
 // En una línea
 // getProducts().then((items => console.log(items)))
+//
+//
